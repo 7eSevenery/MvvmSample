a190823fd61ac3a65f2d6834881a73efb010e165
7eSevenery <7eSevenery@gmail.com>
2023-10-08 09:37:48 +0800

    Add ButtonNavigation

---
+++ ButtonNavigation/ViewModels/NavigationViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonNavigation.ViewModels
+{
+    public partial class NavigationViewModel : ObservableObject
+    {
+        private object _SelectedControl;
+        public object SelectedControl
+        {
+            get => _SelectedControl;
+            set
+            {
+                SetProperty(ref _SelectedControl, value);
+            }
+        }
+
+        public NavigationViewModel()
+        {
+            SelectedControl = new MusicViewModel();
+        }
+
+        [RelayCommand]
+        private void MusicNav()
+        {
+            SelectedControl = new MusicViewModel();
+        }
+
+        [RelayCommand]
+        private void MovieNav()
+        {
+            SelectedControl = new MovieViewModel();
+        }
+
+        [RelayCommand]
+        private void GameNav()
+        {
+            SelectedControl = new GameViewModel();
+        }
+    }
+
+}
